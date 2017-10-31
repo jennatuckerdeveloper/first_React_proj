1bdfdf342dc19351786db9e4c6f1e49e23472724
JennaMTucker <jenna.m.tucker@gmail.com>
2017-10-31 16:06:10 -0700

    updating Git repo structure

--- notes/makedb.js
+++
@@ -1,9 +0,0 @@
-const faker = require('faker')
-
-const randomName = faker.name.findName()
-const randomEmail = faker.internet.email()
-const randomCard = faker.helpers.createCard()
-
-console.log('randomName', randomName)
-console.log('randomEmail', randomEmail)
-console.log('randomCard', randomCard)
